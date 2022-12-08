68d62080af935a81637fb4e5ed351650509d81a2
WellingtonSI <12ribe@gmail.com>
2022-12-07 21:39:23 -0300

    button e começo do display

--- App.js
+++ App.js
@@ -1,117 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from "react"
+import { StyleSheet, View } from "react-native"
+import Button from "./src/components/Button"
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label='AC' />
+          <Button label='/' />
+          <Button label='7' />
+          <Button label='8' />
+          <Button label='9' />
+          <Button label='*' />
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-' />
+          <Button label='1' />
+          <Button label='2' />
+          <Button label='3' />
+          <Button label='+' />
+          <Button label='0' />
+          <Button label='.' />
+          <Button label='=' />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  buttons:{
+    flexDirection:'row',
+    flexWrap: 'wrap'
+  }
+})
---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+
+const styles = StyleSheet.create({
+    display:{
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        alignItems: 'flex-end'
+    },
+    displayValue:{
+
+    }
+})
